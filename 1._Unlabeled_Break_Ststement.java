6d52f476beb027b65b3350083a94296da4b697ac
Sanjit Kumar Singh <62151811+sanjit2312@users.noreply.github.com>
2020-07-16 21:48:32 +0530

    Add files via upload

---
+++ 1._Unlabeled_Break_Ststement.java
@@ -0,0 +1,25 @@
+package labeled_And_Unlabeled;
+
+public class Unlabeled_Break_Ststement 
+{
+
+	public static void main(String[] args)
+	{
+		
+		for(int i=0;i<5;i++)
+		{
+			for(int j=0;j<5;j++)
+			{
+				if(i==3)
+				{
+					break;
+				}
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+
+
+	}
+
+}
