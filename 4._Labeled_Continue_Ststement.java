4612770a3b89ce1d5f0b15967449bc350415a8b9
Sanjit Kumar Singh <62151811+sanjit2312@users.noreply.github.com>
2020-07-16 21:49:41 +0530

    Add files via upload

---
+++ 4._Labeled_Continue_Ststement.java
@@ -0,0 +1,26 @@
+package labeled_And_Unlabeled;
+
+public class Labeled_Continue_Ststement 
+{
+
+	public static void main(String[] args)
+	{
+		
+		Sanjit:
+			for(int i=0;i<5;i++)
+			{
+				for(int j=0;j<5;j++)
+				{
+					if(i==3)
+					{
+						continue Sanjit;
+					}
+					System.out.print("* ");
+				}
+				System.out.println();
+			}
+
+
+	}
+
+}
