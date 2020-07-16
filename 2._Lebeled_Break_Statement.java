0f3b07d7a4ab33943ed9b61ef0f4f267425fbe0f
Sanjit Kumar Singh <62151811+sanjit2312@users.noreply.github.com>
2020-07-16 21:48:53 +0530

    Add files via upload

---
+++ 2._Lebeled_Break_Statement.java
@@ -0,0 +1,26 @@
+package labeled_And_Unlabeled;
+
+public final class Lebeled_Break_Statement 
+{
+
+	public static void main(String[] args)
+	{
+		
+		Sanjit:
+		for(int i=0;i<5;i++)
+		{
+			for(int j=0;j<5;j++)
+			{
+				if(i==3)
+				{
+					break Sanjit;
+				}
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+
+
+	}
+
+}
